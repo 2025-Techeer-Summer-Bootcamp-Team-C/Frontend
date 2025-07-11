841b9ad26a6fc0755167492f48d0eac376d541a6
lazyJSH <lazy.jsh03@gmail.com>
2025-07-12 02:14:02 +0900

    chore: 의존성, 라이브러리 설치, 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,19 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import Layout from "./components/common/Layout";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
+  const queryClient = new QueryClient();
+
   return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Layout>
+    <QueryClientProvider client={queryClient}>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Layout>
+    </QueryClientProvider>
   );
 }
 
