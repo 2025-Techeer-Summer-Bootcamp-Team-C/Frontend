12fba034291cd35db1b6d8741fe29c3946ed30a2
lazyjsh03 <lazy.jsh03@gmail.com>
2025-07-22 17:09:55 +0900

    fix: 빌드 버그 수정

--- src/components/VideoSection.tsx
+++ src/components/VideoSection.tsx
@@ -2,13 +2,10 @@ import { useState, useRef, useEffect } from 'react';
 import video1 from "../assets/video1.mp4";
 import video2 from "../assets/video2.mp4";
 import video3 from "../assets/video3.mp4";
-import { FastAverageColor } from 'fast-average-color';
 
 interface VideoSectionProps {
   onVolumeChange?: (volume: number) => void;
 }
-
-const fac = new FastAverageColor();
 
 const VideoSection = ({ onVolumeChange }: VideoSectionProps) => {
   const [currentVideo, setCurrentVideo] = useState(0);
